60d9b20859072d886d838302ddf80bcf06f74c7f
Alex Schmidt <alex.schmidt767@gmail.com>
2023-02-01 21:10:01 -0600

    binary search with iteration coplete

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,17 @@
-# code your iterative algorithm here
+def binary_search(data, el):
+    first = 0
+    last = len(data) - 1
+    found = False
+    while not found and first <= last:
+        mid = (first + last) // 2
+        if data[mid] == el:
+            found = True
+        elif data[mid] > el:
+            last = mid - 1
+        else:
+            first = mid + 1
+    return found
 
+test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+
+print(binary_search(test_list, 12))
